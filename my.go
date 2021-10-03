8ced6d26a2ac7d48be16ea88caf59d90955c3cca
AURORA\micha <keine@nix.nix>
2021-10-03 20:51:51 +0200

    json report eingefügt.

--- my.go
+++ my.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/fs"
 	"os"
@@ -14,7 +15,24 @@ var allpaths []string
 var dur1 time.Duration = time.Duration(0)
 var dur2 time.Duration = time.Duration(0)
 
+type reportHead struct {
+	Reports []report
+}
+
+type report struct {
+	Extension string
+	Bytesize  int64
+	Loadtime  int64
+	Drawtime  int64
+}
+
+var rh reportHead
+
 func main() {
+
+	rh = reportHead{
+		Reports: []report{},
+	}
 
 	if len(os.Args) != 2 {
 		fmt.Println("Bitte genau einen Pfad zu einer Bildersammlung angeben.")
@@ -25,10 +43,21 @@ func main() {
 
 	filepath.WalkDir(path, walker2)
 
+	var r report
+
 	for _, p := range allpaths {
-		walker(p)
+		r = walker(p)
+		if r.Extension == "---" {
+			continue
+		}
+		rh.Reports = append(rh.Reports, r)
 	}
 
+	result, _ := json.Marshal(rh)
+
+	st := string(result)
+
+	fmt.Println(st)
 	fmt.Println(dur1)
 	fmt.Println(dur2)
 
@@ -41,12 +70,12 @@ func walker2(path string, info fs.DirEntry, err error) error {
 	return nil
 }
 
-func walker(pathname string) {
+func walker(pathname string) report {
 
 	s1 := time.Now()
 	i, err := gg.LoadImage(pathname)
 	if err != nil {
-		return
+		return report{Extension: "---"}
 	}
 	s2 := time.Now()
 	dc := gg.NewContextForImage(i)
@@ -64,4 +93,19 @@ func walker(pathname string) {
 	dur1 += s2.Sub(s1)
 	dur2 += s3.Sub(s2)
 
+	fi, _ := os.Stat(pathname)
+
+	var bs int64
+	if fi != nil {
+		bs = fi.Size()
+	}
+
+	reportdata := report{
+		Extension: filepath.Ext(pathname),
+		Bytesize:  bs,
+		Loadtime:  s2.Sub(s1).Microseconds(),
+		Drawtime:  s3.Sub(s2).Microseconds(),
+	}
+
+	return reportdata
 }
